fcc27b5102791de600215b916971edcd5edffc61
Mario Marić <m4ri000@gmail.com>
2013-07-06 01:59:18 +0200

    Final version 1.0
    Tested and working.

--- mp32flv.sh
+++ mp32flv.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-# Requirements: ffmpeg
+# Requirements: tr, convert, ffmpeg
 # Usage: mp32flv input.mp3
+
+# Output file - change file extension from .mp3 to .flv
+out=$(echo $1 | tr 'mp3' 'flv')
+
+# Creating black background w/ white text i.e. filename
+bgr=/tmp/background.jpg
+filename=$(echo $1 | tr -d '.mp3' | tr '[:punct:]' ' ')
+convert \
+  -size 320x240 \
+  xc:black -fill white \
+  -gravity center -draw "text 0,0 '$filename'" \
+  "$bgr"
+
+# ffmpeg convert command
+ffmpeg -loop_input -r 1 -i "$bgr" -i "$1" -acodec copy -shortest -qscale 11 -s 320x240 "$out"
+
+# Delete background file
+rm "$bgr"
+
+exit
