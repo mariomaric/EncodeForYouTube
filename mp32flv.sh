9d4c5fbe2ef3b2fb9f8c299757bd5a0ac2197b44
Mario Marić <git@mariomaric.net>
2013-10-05 12:28:05 +0200

    Switching to .mp4 container and PNG background image.

--- mp32flv.sh
+++ mp32flv.sh
@@ -3,10 +3,10 @@
 # Usage: mp32flv input.mp3
 
 # Prepare output file i.e. change file extension from .mp3 to .flv
-out=$(echo "$1" | sed 's/mp3/flv/')
+out=$(echo "$1" | sed 's/mp3/mp4/')
 
-# Prepare background variable / JPEG image
-bgr=/tmp/background.jpg
+# Prepare background variable / PNG image
+bgr=/tmp/background.png
 
 # Remove extension and punctuation marks from filename
 filename=$(echo "$1" | sed 's/\.mp3//' | sed 's/[[:punct:]]/ /g')
@@ -19,7 +19,11 @@ convert \
   "$bgr"
 
 # ffmpeg convert command
-ffmpeg -f image2 -loop 1 -framerate 1 -i "$bgr" -i "$1" -codec:a copy -shortest -q:v 11 -s 426x240 "$out"
+ffmpeg \
+  -f image2 -loop 1 -framerate 1 -i "$bgr" -i "$1" \
+  -c:v libx264 -preset medium -tune stillimage -crf 18 \
+  -c:a copy -shortest \
+  -pix_fmt yuv420p -s 426x240 "$out"
 
 # Delete background file
 rm "$bgr"
