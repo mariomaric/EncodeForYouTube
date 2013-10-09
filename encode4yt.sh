3af13b85689813c891202e4fccfb2320c03fda21
Mario Marić <git@mariomaric.net>
2013-10-09 12:27:40 +0200

    Changed indentation.

--- encode4yt.sh
+++ encode4yt.sh
@@ -20,17 +20,17 @@ filename=$(echo "$1" | sed "s/$ext//" | sed 's/[[:punct:]]/ /g')
 
 # 2.3) Create background image using filename as white text on black background
 convert \
-  -size "$size" \
-  -background black -fill white \
-  -gravity center label:"$filename" \
-  "$bgr"
+    -size "$size" \
+    -background black -fill white \
+    -gravity center label:"$filename" \
+    "$bgr"
 
 # 3) Create video with background image and input audio file
 ffmpeg \
-  -f image2 -loop 1 -framerate 1 -i "$bgr" -i "$1" \
-  -c:v libx264 -preset medium -tune stillimage -crf 18 \
-  -c:a copy -shortest \
-  -pix_fmt yuv420p -s "$size" "$out"
+    -f image2 -loop 1 -framerate 1 -i "$bgr" -i "$1" \
+    -c:v libx264 -preset medium -tune stillimage -crf 18 \
+    -c:a copy -shortest \
+    -pix_fmt yuv420p -s "$size" "$out"
 
 # 4) Delete background image
 rm "$bgr"
