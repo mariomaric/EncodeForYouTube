8f67845fc6344791ac9fdaf06c205fce9f9a917d
Mario Marić <git@mariomaric.net>
2013-11-03 23:33:34 +0100

    Minor usability changes in order to easier reuse or c/p code.

--- encode4yt.sh
+++ encode4yt.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Info: https://github.com/mariomaric/EncodeForYouTube#readme
 
+# 0) Initiate input variable
+input="$1"
+
 # 1) Prepare output file:
 # 1.1) Find out extension of input audio file using Shell Parameter Expansion
-ext="${1##*.}"
+ext="${input##*.}"
 
 # 1.2) Set up extension of output file to mp4
-out="$(echo "$1" | sed "s/$ext/mp4/")"
+output="$(echo "$input" | sed "s/$ext/mp4/")"
 
 # 1.3) Set up size for background image and video
 size="1920x1080"
@@ -16,7 +19,7 @@ size="1920x1080"
 bgr="/tmp/background.png"
 
 # 2.2) Remove extension and punctuation marks from filename
-filename="$(echo "$1" | sed "s/$ext//" | sed 's/[[:punct:]]/ /g')"
+filename="$(echo "$input" | sed "s/$ext//" | sed 's/[[:punct:]]/ /g')"
 
 # 2.3) Create background image using filename as white text on black background
 convert \
@@ -27,12 +30,12 @@ convert \
 
 # 3) Create video with background image and input audio file
 ffmpeg \
-    -f image2 -loop 1 -framerate 1 -i "$bgr" -i "$1" \
+    -f image2 -loop 1 -framerate 1 -i "$bgr" -i "$input" \
     -c:v libx264 -preset medium -tune stillimage -crf 18 \
     -c:a copy -shortest \
-    -pix_fmt yuv420p -s "$size" "$out"
+    -pix_fmt yuv420p -s "$size" "$output"
 
 # 4) Delete background image
 rm "$bgr"
 
-exit
+exit 0
