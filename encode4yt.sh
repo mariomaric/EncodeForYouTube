5d26110d4cf38b17ca9ffa7aa746057b821b5380
Mario Marić <git@mariomaric.net>
2013-11-15 21:39:04 +0100

    Increased program readability.

--- encode4yt.sh
+++ encode4yt.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Info: https://github.com/mariomaric/EncodeForYouTube#readme
 
+
+
 # 0) Initiate input variable
 input="$1"
 
+
+
 # 1) Prepare output file:
+
 # 1.1) Find out extension of input audio file using Shell Parameter Expansion
 ext="${input##*.}"
 
@@ -13,6 +18,8 @@ output="$(echo "$input" | sed "s/$ext/mp4/")"
 
 # 1.3) Set up size for background image and video
 size="1920x1080"
+
+
 
 # 2) Prepare background image:
 # 2.1) Create variable with PNG image location
@@ -29,12 +36,16 @@ convert \
     -gravity center label:"$filename" \
     "$bgr"
 
+
+
 # 3) Create video with background image and input audio file
 ffmpeg \
     -f image2 -loop 1 -framerate 1 -i "$bgr" -i "$input" \
     -c:v libx264 -preset medium -tune stillimage -crf 18 \
     -c:a copy -shortest \
     -pix_fmt yuv420p -s "$size" "$output" -loglevel fatal
+
+
 
 # 4) Delete background image
 rm "$bgr"
